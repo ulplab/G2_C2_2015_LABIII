4f9a3ca1827b597740e1147bfa09fdc49c5c8962
arriolamario <arriola.mario.90@gmail.com>
2015-10-31 20:10:20 -0300

    manejador cuota

--- CapaDatos/clsManejadorCuota.cs
+++ CapaDatos/clsManejadorCuota.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Clases;
 using Interfaces;
+using System.Data;
 
 namespace CapaDatos
 {
@@ -74,6 +75,63 @@ namespace CapaDatos
         public List<clsCuota> SelectCuota(clsCuota entidad)
         {
             List<clsCuota> list = new List<clsCuota>();
+            bool cuota = false, alumno = false, curso = false;
+            DataTable dt;
+
+            string query = "select * from Cuota where";
+            if (entidad.Id != -1)
+            {
+                query += " IdCuota= " + entidad.Id;
+                cuota = true;
+            }
+            if (entidad.IdAlumno != -1)
+            {
+                if (cuota)
+                {
+                    query += " and";
+                }
+                query += " IdAlumno= " + entidad.IdAlumno;
+                alumno = true;
+            }
+            if (entidad.IdCurso != -1)
+            {
+                if (cuota | alumno)
+                {
+                    query += " and";
+                }
+                query += " IdCurso= " + entidad.IdCurso;
+                curso = true;
+            }
+            if (entidad.Fecha != null)
+            {
+                if (cuota | alumno | curso)
+                {
+                    query += " and";
+                }
+                query += " Fecha =" + String.Format("{0:s}", entidad.Fecha);
+            }
+
+            try
+            {
+                dt = dbManager.Consultar(query);
+            }
+            catch (Exception e)
+            {
+                
+                throw e;
+            }
+
+            foreach (DataRow item in dt.Rows)
+            {
+                clsCuota c = new clsCuota();
+
+                c.Id = Convert.ToInt32(item["IdCuota"]);
+                c.IdAlumno = Convert.ToInt32(item["IdAlumno"]);
+                c.IdCurso = Convert.ToInt32(item["IdCurso"]);
+                c.Fecha = Convert.ToDateTime(item["Fecha"]);
+
+                list.Add(c);
+            }
 
             return list;
         }
@@ -81,6 +139,30 @@ namespace CapaDatos
         public List<clsCuota> ListarCuota()
         {
             List<clsCuota> list = new List<clsCuota>();
+            DataTable dt;
+            string query = "select * from Cuota";
+
+            try
+            {
+                dt = dbManager.Consultar(query);
+            }
+            catch (Exception e)
+            {
+                
+                throw e;
+            }
+
+            foreach (DataRow item in dt.Rows)
+            {
+                clsCuota c = new clsCuota();
+
+                c.Id = Convert.ToInt32(item["IdCuota"]);
+                c.IdAlumno = Convert.ToInt32(item["IdAlumno"]);
+                c.IdCurso = Convert.ToInt32(item["IdCurso"]);
+                c.Fecha = Convert.ToDateTime(item["Fecha"]);
+
+                list.Add(c);
+            }
 
             return list;
         }
