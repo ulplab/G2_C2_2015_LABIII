0d266893e5e50f1d508eb05f0588f54e8d22b34b
ggpq22 <ggpq13@hotmail.com>
2015-11-01 23:45:59 -0300

    los reportes funcionan bien

--- CapaPresentacion/Reportes/frmCertificadoFinCurso.Designer.cs
+++ CapaPresentacion/Reportes/frmCertificadoFinCurso.Designer.cs
@@ -44,7 +44,7 @@
             this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
             this.crystalReportViewer1.Name = "crystalReportViewer1";
             this.crystalReportViewer1.ReportSource = this.crCertificadoFinCurso1;
-            this.crystalReportViewer1.Size = new System.Drawing.Size(1179, 750);
+            this.crystalReportViewer1.Size = new System.Drawing.Size(1179, 741);
             this.crystalReportViewer1.TabIndex = 0;
             this.crystalReportViewer1.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None;
             // 
@@ -52,7 +52,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1179, 750);
+            this.ClientSize = new System.Drawing.Size(1179, 741);
             this.Controls.Add(this.crystalReportViewer1);
             this.Name = "frmCertificadoFinCurso";
             this.Text = "frmCertificadoFinCurso";
